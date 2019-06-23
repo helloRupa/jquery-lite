b0107c64313f720b44101008f27601eb19c93e86
Rupa Dhillon <rupa211984@gmail.com>
2019-06-22 18:31:39 -0700

    Make AJAX request

--- src/index.js
+++ src/index.js
@@ -22,3 +22,39 @@ $l.extend = function(...args) {
 
   return merged;
 }
+
+$l.ajax = function(options) {
+  let xhr = new XMLHttpRequest();
+
+  const defaults = {
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    method: 'GET',
+    url: '',
+    success: () => {},
+    error: () => {},
+    data: {}
+  };
+  
+  options = $l.extend(defaults, options);
+  options.method = options.method.toUpperCase();
+
+  if (options.method === 'GET') {
+    const queryString = Object.keys(options.data).map((key) => {
+      encodeURIComponent(key) + '=' + encodeURIComponent(options.data[key])
+    }).join('&');
+
+    options.url += queryString;
+  }
+
+  xhr.onload = function() {
+    if (this.status == 200) {
+      options.success(JSON.parse(this.responseText));
+    } else {
+      options.error(this.status);
+    }
+  };
+
+  xhr.open(options.method, options.url, true);
+  xhr.setRequestHeader('Content-type', options.contentType);
+  xhr.send(JSON.stringify(options.data));
+}
